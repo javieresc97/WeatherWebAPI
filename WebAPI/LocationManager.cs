6b1c7154252dc7f0e573be2d8f265ca180ffbd7b
Javier Escobar <javieresc97@hotmail.com>
2016-01-05 00:10:42 -0500

    Initial commit

---
+++ WebAPI/LocationManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace WebAPI
+{
+    class LocationManager
+    {
+        public async static Task<Geoposition> GetPosition()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            if (accessStatus != GeolocationAccessStatus.Allowed)
+            {
+                throw new Exception();
+            }
+
+            //  Ninguna precisión específica requerida
+            var geolocator = new Geolocator { DesiredAccuracyInMeters = 0 };
+
+            //  Posición real actual
+            var position = await geolocator.GetGeopositionAsync();
+
+            return position;
+        }
+    }
+}
